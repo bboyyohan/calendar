c582188411d64271ad65b23ffda4a0dbd34371ca
Injae Lee <github email address>
2020-06-16 16:26:46 -0400

    create-task done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,5 +2,17 @@ class TasksController < ApplicationController
     def index 
         tasks = Task.all 
         render json: tasks
-    end 
+    end
+    
+    def create
+        new_task = Task.create(task_params)
+        byebug
+        render json: new_task.to_json
+    end
+
+    private
+    def task_params
+      params.require(:task).permit(:title, :date, :start_time, :end_time, :description, :completion, :user_id)
+    end
+
 end
